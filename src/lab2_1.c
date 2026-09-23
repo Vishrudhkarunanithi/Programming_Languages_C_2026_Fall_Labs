9a3106e33a0143a88972ac8d3dbda4d87bd8166e
Vishrudhkarunanithi <vishrudh.pattukkottai@edu.rtu.lv>
2026-09-23 11:05:12 +0000

    lab2

--- src/lab2_1.c
+++ src/lab2_1.c
@@ -12,17 +12,29 @@
 */
 
 int sum_to_n(int n) {
-    // TODO: implement sum with a for loop
-    return 0; // placeholder
+  int sum = 0;
+
+  for (int i = 1; i <= n; i++)
+
+  {
+    sum += i;
+  }
+
+  return sum;  // placeholder
 }
 
 int main(void) {
-    int n;
+  int n;
 
-    printf("Enter a positive integer n: ");
-    scanf("%d", &n);
+  printf("Enter a positive integer n: ");
+  scanf("%d", &n);
 
-    // TODO: validate input, call function, and print result
+  if (n < 1) {
+    printf("Error:n must be aleast 1.\n");
 
-    return 0;
+  } else {
+    int result = sum_to_n(n);
+    printf("Sum for %d is %d\n", n, result);
+  }
+  return 0;
 }
